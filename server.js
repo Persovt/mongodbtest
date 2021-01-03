6f1d19e44b4d1174a78feee6a59153ee2feff5c7
Persovt <mza200339@gmail.com>
2021-01-03 19:21:52 +0300

    small fix

--- server.js
+++ server.js
@@ -4,4 +4,6 @@ mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUn
 const Cat = mongoose.model('Cat', { name: String });
 
 const kitty = new Cat({ name: 'Zildjian' });
-kitty.save().then(() => console.log('meow'));
+kitty.save()
+    .then(() => console.log('success'))
+    .catch((err) => console.log('error'))
